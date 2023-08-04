ae2d8be432f6075edea099f19e8efda698d693da
DEVMINWOO-JUNG <applejung312@gmail.com>
2023-08-04 21:34:01 +0900

    asd

--- minstagram/src/app/page.tsx
+++ minstagram/src/app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <section>Minstagram</section>
+    <section>
+
+      
+    </section>
   )
 }
--- minstagram/src/components/ui/Avatar.tsx
+++ minstagram/src/components/ui/Avatar.tsx
@@ -7,6 +7,8 @@ type Props = {
 }
 
 export default function Avatar({image}: Props) {
+  
+
   return (
     <div className='w-9 h-9 rounded-full bg-gradient-to-bl from-white'>
       <img className='rounded-full p-[0.1rem]' alt='user profile' src={image ?? undefined} referrerPolicy='no-referrer'/>
