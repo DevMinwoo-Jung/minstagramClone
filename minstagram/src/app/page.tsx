06b1f535f20af92cdc729aed9d98bb76aee999f0
DEVMINWOO-JUNG <applejung312@gmail.com>
2023-08-05 12:34:21 +0900

    add sidebar following follers componets

--- minstagram/src/app/page.tsx
+++ minstagram/src/app/page.tsx
@@ -1,8 +1,19 @@
+import SideBar from "@/components/SideBar";
+
 export default function Home() {
   return (
     <section>
-
-      
+      <div>
+        <div>
+          FollowingBar
+        </div>
+        <div>
+          PostList
+        </div>
+        <div>
+          <SideBar/>
+        </div>
+      </div>
     </section>
   )
 }
