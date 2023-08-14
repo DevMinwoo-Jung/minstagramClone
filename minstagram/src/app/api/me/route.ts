3b35ea2f7e0878aa65ca0e10b5b898ed20326047
DEVMINWOO-JUNG <applejung312@gmail.com>
2023-08-14 12:24:22 +0900

    add route

---
+++ minstagram/src/app/api/me/route.ts
@@ -0,0 +1,14 @@
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if(!user) {
+    return new Response('Auth Error', { status: 401 })
+  }
+  
+  return NextResponse.json('asd');
+}
