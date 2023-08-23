abfc54f4a33d5ae0443508573bb6009d5177d8e8
DEVMINWOO-JUNG <applejung312@gmail.com>
2023-08-23 20:24:52 +0900

    modify query

--- minstagram/src/service/posts.ts
+++ minstagram/src/service/posts.ts
@@ -1,4 +1,5 @@
-import { client } from "./sanity";
+import { SimplePost } from "@/types/model/post";
+import { client, urlFor } from "./sanity";
 // import imageUrlBuilder from '@sanity/image-url';
 
 // const builder = imageUrlBuilder(client)
@@ -7,9 +8,25 @@ import { client } from "./sanity";
 //   return builder.image(source)
 // }
 
+const simplePostProjection = `
+    ...,
+    "username": author->username,
+    "userImage": author->image,
+    "image": photo,
+    "likes": likes[]->username,
+    "text": comments[0].comment,
+    "comments": count(comments),
+    "id":_id,
+    "createdAt":_createdAt
+`;
+
 export async function getPosts(userName: string) {
-  console.log(userName)
-  return client.fetch(`
-    *[_type == "post"]
-  `);
+  return client
+    .fetch(
+      `*[_type =="post" && author->username == "${userName}"
+          || author._ref in *[_type == "user" && username == "${userName}"].following[]._ref]
+          | order(_createdAt desc){
+          ${simplePostProjection}
+        }`
+    )
 }
--- minstagram/src/types/model/post.ts
+++ minstagram/src/types/model/post.ts
@@ -1,6 +1,20 @@
-export type PostType = {
-  author: string;
-  photo?: string;
+export type Comment = {
+  comment: string;
+  username: string;
+  image: string;
+};
+
+export type SimplePost = Omit<FullPost, 'comments'> & {
+  comments: number;
+};
+
+export type FullPost = {
+  id: string;
+  username: string;
+  userImage: string;
+  image: string;
+  text: string;
+  createdAt: string;
   likes: string[];
-  comments: string;
-}
+  comments: Comment[];
+};
