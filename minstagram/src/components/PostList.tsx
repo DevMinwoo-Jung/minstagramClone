e2c5d7c9e6af2c15d622f1936f62f3cb2f5c3d44
DEVMINWOO-JUNG <applejung312@gmail.com>
2023-08-25 12:23:51 +0900

    add post

--- minstagram/src/components/PostList.tsx
+++ minstagram/src/components/PostList.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { FullPost } from '@/types/model/post';
 import React from 'react'
+import { PropagateLoader } from 'react-spinners';
 import useSWR from 'swr';
+import Post from './Post';
 
 const fetcher = async (url:string) => {
   const res = await fetch(url);
@@ -12,9 +14,24 @@ export default function PostList() {
 
   const { data, isLoading, error } = useSWR<FullPost>('/api/posts', fetcher);
 
-  console.log(data)
+  const post = data;
+
+  console.log(post)
 
   return (
-    <div>PostList</div>
+    <section>
+      {/* {
+        isLoading ? ( <PropagateLoader size={8} color='red'/> ):
+        ((!post) && <p>${`you don"t have following`}</p>
+      )} */}
+      {
+        data && 
+        (
+          <div>
+            <Post/>
+          </div>
+        )
+      }
+    </section>
   )
 }
