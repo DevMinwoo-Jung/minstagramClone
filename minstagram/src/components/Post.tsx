09cff7347e68ecc356f3721493a1f33eb252c089
DEVMINWOO-JUNG <applejung312@gmail.com>
2023-08-28 21:32:19 +0900

    틀 만듬

--- minstagram/src/components/Post.tsx
+++ minstagram/src/components/Post.tsx
@@ -11,31 +11,31 @@ export default function Post(props:Props) {
   const { text, id, userImage, username, image, createdAt,likes, comments } = props.post
 
   return (
-    <section className='w-min-full mt-6 block'>
-      <div className='w-full flex ml-3 items-center'>
+    <section className='m-auto w-2/3 mt-6 block rounded-md border-2'>
+      <div className='w-full flex ml-3 items-center mt-4'>
         <Avatar image={userImage}/>
         <span className='ml-6'>{username}</span>
       </div>
       <div className='h-96'>
-        asd
+        <image/>
       </div>
-      <div className='w-full'>
-        <div className='justify-end'>
-          <span>Likes</span>
-          <span>Heart</span>
+      <div className='w-full mt-3'>
+        <div className='flex justify-between'>
+          <span className='ml-6'>Likes</span>
+          <span className='mr-6'>Heart</span>
         </div>
-        <div className='block'>
+        <div className='block mt-3'>
           <p className='ml-6'>0 Like</p>
-          <div>
+          <div className='ml-6 mt-3'>
             <span className='font-extrabold'>{username}</span>
             <span>{text}</span>
           </div>
         </div>
       </div>
-      <div className='bg-white flex'>
-        <span>Smile</span>
-        <input type="text" placeholder='Add a comment' />
-        <span className='ring-offset-blue'>Post</span>
+      <div className='bg-white flex mt-3 mb-4'>
+        <span className='ml-4 mr-4'>Smile</span>
+        <input className='w-full' type="text" placeholder='Add a comment' />
+        <span className='ring-offset-blue mr-4'>Post</span>
       </div>
     </section>
   )
