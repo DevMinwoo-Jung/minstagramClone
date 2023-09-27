4192da08a5baf41d9c63abab676cf1f300da84b2
DEVMINWOO-JUNG <applejung312@gmail.com>
2023-09-27 23:01:37 +0900

    add usercard

--- minstagram/src/app/search/page.tsx
+++ minstagram/src/app/search/page.tsx
@@ -3,13 +3,13 @@ import SearchBar from '@/components/SearchBar';
 import UserCard from '@/components/UserCard';
 import { DetailUser } from '@/types/model/user';
 import React from 'react'
+import { CgSearchLoading } from 'react-icons/cg';
 import useSWR from 'swr';
 
 const SearchPage = () => {
 
-    const {data: users, isLoading: loading} = useSWR<DetailUser>('/api/search');
+  const {data: users, isLoading: loading} = useSWR<DetailUser[]>('/api/search');
 
-    console.log(users)
 
   return (
     <section className='w-3/4 m-auto justify-center items-center'>
@@ -17,7 +17,22 @@ const SearchPage = () => {
         <SearchBar/>
       </div>
       <div className='m-auto'>
-        <UserCard/>
+        {
+          loading  && <CgSearchLoading/>
+        }
+        {
+          !loading && 
+          users!.map((user)=> {
+
+            const { username, image, name, followers, following } = user;
+
+            return (
+              <>
+                <UserCard username={username} image={image!} name={name} followers={followers} following={following}/>  
+              </>
+            )
+          })
+        }
       </div>
     </section>
   )
--- minstagram/src/components/UserCard.tsx
+++ minstagram/src/components/UserCard.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import Avatar from './ui/Avatar'
 
-export default function UserCard() {
+type Props = {
+  username: string;
+  image: string;
+  name: string;
+  followers: any;
+  following: any;
+}
+
+export default function UserCard(user:Props) {
+
+  const { username, image, name, followers, following } = user;
+
   return (
-    <div>UserCard</div>
+    <div className='flex'>
+      <Avatar image={image}/>
+      <div className='ml-4'>
+          <p>{username}</p>
+          <p>{name}</p>
+          <span>{followers}followers</span>
+          <span>{following}following</span>
+      </div>
+    </div>
   )
 }
