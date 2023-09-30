777ab91cc29a848f7d690e12dc208cc8fed6d438
DEVMINWOO-JUNG <applejung312@gmail.com>
2023-09-30 21:46:51 +0900

    add Link

--- minstagram/src/components/UserCard.tsx
+++ minstagram/src/components/UserCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Avatar from './ui/Avatar'
+import Link from 'next/link';
 
 type Props = {
   username: string;
@@ -14,14 +15,16 @@ export default function UserCard(user:Props) {
   const { username, image, name, followers, following } = user;
 
   return (
-    <div className='flex rounded-lg border-2 mt-2 mb-2 p-2'>
-      <Avatar image={image}/>
-      <div className='ml-4'>
-          <p className='font-bold text-xl'>{username}</p>
-          <p className='mt-2 mb-2'>{name}</p>
-          <span className='text-gray-300 ml-2'>{(followers === null) ? 0 : followers} followers</span>
-          <span className='text-gray-300'>{(following === null) ? 0 : following} following</span>
+    <Link href={username}>
+      <div className='flex rounded-lg border-2 mt-2 mb-2 p-2 cursor-pointer'>
+        <Avatar image={image}/>
+        <div className='ml-4'>
+            <p className='font-bold text-xl'>{username}</p>
+            <p className='mt-2 mb-2'>{name}</p>
+            <span className='text-gray-300 ml-2'>{(followers === null) ? 0 : followers} followers</span>
+            <span className='text-gray-300'>{(following === null) ? 0 : following} following</span>
+        </div>
       </div>
-    </div>
+    </Link>
   )
 }
