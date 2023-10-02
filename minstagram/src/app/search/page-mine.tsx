cd63f152826741338aeb5424b83059086eee26ca
DEVMINWOO-JUNG <applejung312@gmail.com>
2023-10-02 22:07:36 +0900

    search component

---
+++ minstagram/src/app/search/page-mine.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { DetailUser } from '@/types/model/user';
+import React, { useEffect, useState } from 'react'
+import useSWR from 'swr';
+
+const SearchPageMine = () => {
+
+  const [param, setParam] = useState('');
+
+  return (
+    <section className=''>
+
+    </section>
+  )
+}
+
+export default SearchPageMine
