e22a7abbbaa6ff21bb81629c8ed16c9c314f8112
DEVMINWOO-JUNG <applejung312@gmail.com>
2023-10-13 22:12:51 +0900

    add style

--- minstagram/src/components/userProfile.tsx
+++ minstagram/src/components/userProfile.tsx
@@ -6,37 +6,29 @@ type Props = {
   user: UserProfile;
 };
 export default function UserProfile({ user }: Props) {
-  const { image, username ,name, followers, following, posts } = user;
+  const { image, username, name, followers, following, posts } = user;
   const info = [
-    {
-      title: 'posts', data: posts
-    },
-    {
-      title: 'followers', data: followers
-    },
-    {
-      title: 'following', data: following
-    }
-  ]
-  console.log(user)
+    { title: 'posts', data: posts },
+    { title: 'followers', data: followers },
+    { title: 'following', data: following },
+  ];
   return (
-    <section className="flex justify-center w-full pt-6 pb-6 h-32">
-      <Avatar image={image} highlight />
-      <div>
-        <h1>{username}</h1>
-        <FollowButton user={user}/>
-        <ul>
-          {
-            info.map(({title, data}, index) => 
-            (
+    <section className='w-full flex flex-col md:flex-row items-center justify-center py-12 border-b border-neutral-300'>
+      <Avatar image={image} highlight size='large' />
+      <div className='md:ml-10 basis-1/3'>
+        <div className='flex flex-col items-center md:flex-row'>
+          <h1 className='text-2xl md:mr-8 my-2 md:mb-0'>{username}</h1>
+          <FollowButton user={user} />
+        </div>
+        <ul className='my-4 flex gap-4'>
+          {info.map(({ title, data }, index) => (
             <li key={index}>
-              <span>{title}</span>
-              <span>{data}</span>
+              <span className='font-bold mr-1'>{data}</span>
+              {title}
             </li>
-            ))
-          }
+          ))}
         </ul>
-        <p>name</p>
+        <p className='text-xl font-bold text-center md:text-start'>{name}</p>
       </div>
     </section>
   );
